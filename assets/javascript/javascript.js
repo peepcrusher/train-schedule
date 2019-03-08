787ccbc042a122cadb6c4415214ea162c76fd7a7
Sam Thompson <electronic62794@yahoo.com>
2019-03-07 21:46:13 -0800

    first version of the working app

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -12,7 +12,10 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-
+var trainName = ""
+var trainDestination = ""
+var firstTrain = 0
+var trainFrequency = 0
 
 
 
@@ -22,30 +25,61 @@ var database = firebase.database();
 $("#submit-train").on("click", function (event) {
     event.preventDefault();
 
-    //create variables that will hook onto the information that the user Submits 
-    var trainName = $("#train-name").val()
-    var trainDestination = $("#train-destination").val()
-    var firstTrain = $("#first-train").val()
-    var trainFrequency = $("#frequency").val()
 
+
+    //create variables that will hook onto the information that the user Submits 
+     trainName = $("#train-name").val()
+     trainDestination = $("#train-destination").val()
+     firstTrain = $("#first-train").val()
+     trainFrequency = $("#frequency").val()
+
+
+    //convert first train into time usable for data
     var firstConvertedTime = moment(firstTrain, "HH:mm");
     console.log(firstConvertedTime);
-
+    //the current time
     var currentTime = moment();
-console.log("Current time: " + currentTime.format("hh:mm"));
+    console.log("Current time: " + currentTime.format("hh:mm"));
+    //the difference in minutes between the current time, and the time of the first train
     var diffTime = currentTime.diff(moment(firstConvertedTime), "minutes");
-console.log(diffTime);
+    console.log(diffTime);
+
+    //calculate the remainder between the train frequency and the time that has passed since the first train
     var remainder = diffTime % trainFrequency;
-console.log(remainder + "remainder");
+    console.log(remainder + "remainder");
+    //calculate the time until the next train
     var tillNextTrain = trainFrequency - remainder;
-console.log(tillNextTrain + " minutes till next train");
+    console.log(tillNextTrain + " minutes till next train");
+
+    //calculate what time the next train will arrive
     var nextTrain = currentTime.add(tillNextTrain, "minutes");
-console.log("arrival time" + nextTrain.format("hh:mm"));
-    
+    console.log("arrival time" + nextTrain.format("hh:mm"));
+
+
+    database.ref().push({
+        TrainName: trainName,
+        trainDestination: trainDestination,
+        frequency: trainFrequency,
+        nextArrival: nextTrain.format("hh:mm:A").toString(),
+        tillNextTrain: tillNextTrain,
+    });
+
+   $(".train-inputs").reset();
+});
+
+//create a listener for on_child
+database.ref().on("child_added", function (snapshot) {
+    //append the database data to the DOM in the form of a table
+    $("#rows-and-data-here").append(
+        "<tr>" +
+        "<td>" + snapshot.val().TrainName + "</td>" +
+        "<td>" + snapshot.val().trainDestination + "</td>" +
+        "<td>" + snapshot.val().frequency + "</td>" +
+        "<td>" + snapshot.val().nextArrival + "</td>" +
+        "<td>" + snapshot.val().tillNextTrain + "</td>" +
+        "</tr>"
+    )
 })
-
-//take the user information and the new next train and next arrival variables, and store it in a firebase folder
-
 
 
 //display the information stored in firebase, to the DOM
